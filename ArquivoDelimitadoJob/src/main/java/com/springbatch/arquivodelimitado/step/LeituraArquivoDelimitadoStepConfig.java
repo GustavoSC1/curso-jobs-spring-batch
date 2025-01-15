d6a7199331b788854c2bb9abac7c945684e29123
GustavoSC1 <gu.cruz17@hotmail.com>
2025-01-15 20:58:01 -0300

    Leitura de arquivo delimitado

---
+++ ArquivoDelimitadoJob/src/main/java/com/springbatch/arquivodelimitado/step/LeituraArquivoDelimitadoStepConfig.java
@@ -0,0 +1,33 @@
+package com.springbatch.arquivodelimitado.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.springbatch.arquivodelimitado.dominio.Cliente;
+
+@Configuration
+public class LeituraArquivoDelimitadoStepConfig {
+	
+	@Autowired
+	public JobRepository jobRepository;
+	
+	@Autowired
+	public PlatformTransactionManager platformTransactionManager;
+	
+	@Bean
+	public Step leituraArquivoDelimitadoStep(ItemReader<Cliente> leituraArquivoDelimitadoReader,
+			ItemWriter<Cliente> leituraArquivoDelimitadoWriter) {
+		return new StepBuilder("leituraArquivoDelimitadoStep", jobRepository)
+				.<Cliente, Cliente>chunk(1, platformTransactionManager)
+				.reader(leituraArquivoDelimitadoReader)
+				.writer(leituraArquivoDelimitadoWriter)
+				.build();
+	}
+}
