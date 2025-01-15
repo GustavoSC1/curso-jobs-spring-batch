51d2a141ee4cae12bae9421ea9ae1848bf636d3b
GustavoSC1 <gu.cruz17@hotmail.com>
2025-01-15 19:11:14 -0300

    Leitura de arquivo de largura fixa

---
+++ ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/step/LeituraArquivoLarguraFixaStepConfig.java
@@ -0,0 +1,33 @@
+package com.springbatch.arquivolargurafixa.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.springbatch.arquivolargurafixa.dominio.Cliente;
+
+@Configuration
+public class LeituraArquivoLarguraFixaStepConfig {
+	
+	@Autowired
+	public JobRepository jobRepository;
+	
+	@Autowired
+	public PlatformTransactionManager platformTransactionManager;
+	
+	@Bean
+	public Step leituraArquivoLarguraFixaStep(ItemReader<Cliente> leituraArquivoLarguraFixaReader,
+			ItemWriter<Cliente> leituraArquivoLarguraFixaWriter) {
+		return new StepBuilder("leituraArquivoLarguraFixaStep", jobRepository)
+				.<Cliente, Cliente>chunk(1, platformTransactionManager)
+				.reader(leituraArquivoLarguraFixaReader)
+				.writer(leituraArquivoLarguraFixaWriter)
+				.build();
+	}
+}
--- primeiroprojetospringbatch/src/main/java/com/gustavo/primeiroprojetospringbatch/job/BatchConfig.java
+++ primeiroprojetospringbatch/src/main/java/com/gustavo/primeiroprojetospringbatch/job/BatchConfig.java
@@ -7,17 +7,14 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.support.JdbcTransactionManager;
 
 @Configuration
 public class BatchConfig {
 	
 	public final JobRepository jobRepository;
-	public final JdbcTransactionManager transactionManager;
 		
-	public BatchConfig(JobRepository jobRepository, JdbcTransactionManager transactionManager) {
+	public BatchConfig(JobRepository jobRepository) {
 		this.jobRepository = jobRepository;
-		this.transactionManager = transactionManager;
 	}
 	
 	@Bean
