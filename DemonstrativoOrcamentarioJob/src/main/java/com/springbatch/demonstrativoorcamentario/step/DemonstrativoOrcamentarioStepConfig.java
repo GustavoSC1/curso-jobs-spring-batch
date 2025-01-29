90914044e698ba422c0fb1ae3b89b06f1666f447
GustavoSC1 <gu.cruz17@hotmail.com>
2025-01-29 16:09:07 -0300

    Escrita em arquivo flat customizado

---
+++ DemonstrativoOrcamentarioJob/src/main/java/com/springbatch/demonstrativoorcamentario/step/DemonstrativoOrcamentarioStepConfig.java
@@ -0,0 +1,37 @@
+package com.springbatch.demonstrativoorcamentario.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
+import com.springbatch.demonstrativoorcamentario.reader.GrupoLancamentoReader;
+
+@Configuration
+public class DemonstrativoOrcamentarioStepConfig {
+	
+	@Autowired
+	public JobRepository jobRepository;
+	
+	@Autowired
+	public PlatformTransactionManager platformTransactionManager;
+	
+	@Bean
+	public Step demonstrativoOrcamentarioStep(
+			// Esse aqui lê dos arquivos
+			//MultiResourceItemReader<GrupoLancamento> demonstrativoOrcamentarioReader,
+			// Esse aqui lê do banco de dados
+			GrupoLancamentoReader demonstrativoOrcamentarioReader,
+			ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter) {
+		return new StepBuilder("demonstrativoOrcamentarioStep", jobRepository)
+				.<GrupoLancamento,GrupoLancamento>chunk(100, platformTransactionManager)
+				.reader(demonstrativoOrcamentarioReader)
+				.writer(demonstrativoOrcamentarioWriter)
+				.build();
+	}
+}
